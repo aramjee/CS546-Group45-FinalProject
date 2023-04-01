284c9e8b50cf29bf4051613a61fc4a8d7350cd80
XinxuanLyu <xlyu5@stevens.edu>
2023-03-31 23:09:12 -0400

    Gym data code

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 import * as user from  "./data/user.js";
+import * as gym from  "./data/gym.js";
+
 import {dbConnection, closeConnection} from './config/mongoConnection.js';
 
 await main();
@@ -14,9 +16,18 @@ async function main() {
   testUser = await user.create("Pink", "Floyd", "UsernameTest", "email@gg.com", "North Bergen", "NJ", "04/26/1994", 1, "asdmoa1!%asd");
   console.log(testUser);
   // console.log(testUser._id)
+  testUser.firstName = "aaa"
+  await user.update(testUser._id, testUser)
   console.log('-------------------------------------------');
-  await user.update(testUser._id, "Blue", "Floyd", "UsernameTest", "email@gg.com", "North Bergen", "NJ", "04/26/1994", 1, "asdmoa1!%asd")
+  console.log(await user.getAll());
+  let testGym = await gym.create("TestGym", "https://www.netflix.com", "Training", testUser._id, "Add", "Cit", "NJ", "07047");
+  console.log(await gym.getAll());
+  console.log(await user.getAll());
   console.log('-------------------------------------------');
+  testGym.gymName = 'AAA';
+  await gym.update(testGym._id, testGym);
+  console.log(await gym.getAll());
+  await gym.remove(testGym._id);
   console.log(await user.getAll());
   await closeConnection();
   console.log('closeConnection');
--- data/gym.js
+++ data/gym.js
@@ -1,3 +1,132 @@
 // CS546 group 45 final project
 // team members:Amit Ramjee, Chuqing Ke, Gabriel Souza, Xinxuan Lyu
-// placeholder: API GoogleDoc link 
+// placeholder: API GoogleDoc link
+
+import {ObjectId} from "mongodb";
+import * as validation from '../public/validation.js';
+import {gymCollection} from '../config/mongoCollections.js';
+import * as userFunctions from './user.js'
+
+const create = async (
+  gymName,
+  website,
+  category,
+  gymOwnerId,
+  address,
+  city,
+  state,
+  zip
+) => {
+  // Validation
+  await validation.checkArgumentsExist(gymName, website, category, gymOwnerId, address, city, state, zip);
+  await validation.checkNonEmptyStrings(gymName, website, category, gymOwnerId, address, city, state, zip);
+  await validation.checkValidWebsite(website);
+
+  const newGym = {
+    gymName: gymName.trim(),
+    website: website.trim(),
+    category: category.trim(),
+    gymOwnerId: gymOwnerId.trim(),
+    address: address.trim(),
+    city: city.trim(),
+    state: state.trim(),
+    zip: zip.trim(),
+    reviews: [],
+    likedGymsCnt: 0,
+    dislikedGymsCnt: 0,
+    rating: 0
+  };
+
+  const gymsDBConnection = await gymCollection();
+  const insertInfo = await gymsDBConnection.insertOne(newGym);
+  if (!insertInfo.acknowledged || !insertInfo.insertedId) {
+    throw `Error: Create gym failed`;
+  }
+  const newId = insertInfo.insertedId.toString();
+  let user = await userFunctions.getByUserId(gymOwnerId)
+  user.gymsListForOwner.push(newId);
+  await userFunctions.update(gymOwnerId, user)
+  const gym = await getByGymId(newId);
+  return gym
+};
+
+const getByGymId = async (id) => {
+  await validation.checkObjectId(id);
+  const gymsDBConnection = await gymCollection();
+  const gymGet = await gymsDBConnection.findOne({_id: ObjectId(id.trim())});
+  if (gymGet === null)
+    throw [404, `ERROR: No gym exists with that id ${id.toString()}`];
+  gymGet._id = gymGet._id.toString();
+  return gymGet;
+};
+
+const getAll = async () => {
+  const gymsDBConnection = await gymCollection();
+  let gymList = await gymsDBConnection.find({}).toArray();
+  gymList = gymList.map((element) => {
+    element._id = element._id.toString();
+    return element;
+  });
+  return gymList;
+};
+
+
+const remove = async (id) => {
+  await validation.checkObjectId(id);
+  const gymsDBConnection = await gymCollection();
+  const gym = await getByGymId(id);
+  const userId = gym.gymOwnerId;
+  const deletionInfo = await gymsDBConnection.findOneAndDelete({
+    _id: ObjectId(id.trim())
+  });
+  let user = await userFunctions.getByUserId(userId);
+  user.gymsListForOwner = user.gymsListForOwner.filter((gymId) => gymId !== id);
+  await userFunctions.update(userId, user);
+
+  if (deletionInfo.lastErrorObject.n === 0) {
+    throw [404, `Could not delete gym with Id ${id}`];
+  }
+  return {
+    "bandId": id,
+    "deleted": true
+  };
+};
+
+const update = async (id, gym) => {
+  // Validation
+  await validation.checkObjectId(id);
+  await validation.checkArgumentsExist(gym.gymName, gym.website, gym.category, gym.gymOwnerId, gym.address, gym.city, gym.state, gym.zip);
+  await validation.checkNonEmptyStrings(gym.gymName, gym.website, gym.category, gym.gymOwnerId, gym.address, gym.city, gym.state, gym.zip);
+  await validation.checkValidWebsite(gym.website);
+  await validation.checkValidNonNegativeInteger(gym.likedGymsCnt);
+  await validation.checkValidNonNegativeInteger(gym.dislikedGymsCnt);
+  await validation.checkValidRating(gym.rating)
+
+  // Update the band data in the database
+  const gymsDBConnection = await gymCollection();
+  const updateInfo = await gymsDBConnection.findOneAndUpdate(
+    {_id: new ObjectId(id.trim())},
+    {$set: {
+        gymName: gym.gymName,
+        website: gym.website,
+        category: gym.category,
+        gymOwnerId: gym.gymOwnerId,
+        address: gym.address,
+        city: gym.city,
+        state: gym.state,
+        zip: gym.zip,
+        reviews: gym.reviews,
+        likedGymsCnt: gym.likedGymsCnt,
+        dislikedGymsCnt: gym.dislikedGymsCnt,
+        rating: gym.rating
+      }},
+    {returnDocument: 'after'}
+  );
+
+  if (updateInfo.lastErrorObject.n === 0)
+    throw [404, `Error: Update failed, user not found ${id}`];
+
+  return updateInfo.value;
+};
+
+export {create, getAll, getByGymId, update, remove}
