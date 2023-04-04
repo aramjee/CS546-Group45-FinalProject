cb4ab7c083779c1664c59a302244c78af606d3bf
Amit Ramjee <amitramjee@gmail.com>
2023-04-04 13:04:55 -0400

    Added routes, handlebars, express, index file, etc to setup project to view initial page

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,21 @@
 //Here you will import route files and export them as used in previous labs
+
+//Here you will import route files and export them as used in previous labs
+import gymRoutes from './gym.js';
+import userRoutes from './user.js';
+import commentRoutes from './comment.js';
+import reviewRoutes from './review.js';
+
+const constructorMethod = (app) => {
+  //app.use('/posts', postRoutes);
+
+  app.use('/gym', gymRoutes);
+  app.use('/user', userRoutes);
+  app.use('/comment', commentRoutes);
+  app.use('/review', reviewRoutes);
+  app.use('*', (req, res) => {
+    res.status(404).render('error', { error: '404 : Page not Found' });
+  });
+};
+
+export default constructorMethod;
